200d8a8f3f5c849338319670c5eeb0096aae27a5
abraham2212 <ibrahimsak@code.edu.az>
2023-04-11 23:04:41 +0400

    task

---
+++ Asp.net practices, Elearn template/Areas/Admin/Controllers/CourseController.cs
@@ -0,0 +1,44 @@
+﻿using ElearnApp.Data;
+using ElearnApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElearnApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CourseController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CourseController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Course> courses = await _context.Courses
+                .Include(c=>c.CourseImages)
+                .Include(c=>c.Author)
+               .Where(s => !s.SoftDelete)
+               .ToListAsync();
+              
+
+            return View(courses);
+        }
+        public async Task<IActionResult> Detail()
+        {
+          Course course = await _context.Courses
+                .Include(c => c.CourseImages)
+                .Include(c => c.Author)
+               .Where(s => !s.SoftDelete)
+               .FirstOrDefaultAsync();
+
+
+            return View(course);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Asp.net practices, Elearn template/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ElearnApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElearnApp.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<CourseImage> CourseImages { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<News> News { get; set; }
+    }
+}
